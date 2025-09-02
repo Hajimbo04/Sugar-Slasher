3c5d0092f913af4bf44ad42145d0a853a328375f
Miisan <azarmisan@gmail.com>
2025-09-02 15:36:58 +0800

    feat: health slider smoothening

--- MGGJ_12/Assets/Scripts/Health & Damage System/HealthBar.cs
+++ MGGJ_12/Assets/Scripts/Health & Damage System/HealthBar.cs
@@ -4,14 +4,22 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider healthSlider;
+    float targetValue;
+
+    void Update()
+    {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, targetValue, Time.deltaTime * 10f);
+    }
 
     public void SetSlider(float amount)
     {
-        healthSlider.value = amount;
+        targetValue = amount;
     }
+
     public void SetSliderMax(float amount)
     {
         healthSlider.maxValue = amount;
-        SetSlider(amount);
+        targetValue = amount;
+        healthSlider.value = amount;
     }
 }
